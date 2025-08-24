e6b81b47c6865f35adf5f3dd190d18dcdfa8c66c
Jenna Yang <jmy001@ucsd.edu>
2025-08-24 08:51:04 -0400

    Initial Commit

---
+++ Finance Tracker/Finance_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_TrackerApp.swift
+//  Finance Tracker
+//
+//  Created by Nick Mak on 8/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Finance_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Finance TrackerTests/Finance_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_TrackerTests.swift
+//  Finance TrackerTests
+//
+//  Created by Nick Mak on 8/24/25.
+//
+
+import Testing
+@testable import Finance_Tracker
+
+struct Finance_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
